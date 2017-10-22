af846141fd8781f44b878be924c87dfab8a39e74
Samuel Ogden <samuelogden@Samuels-MacBook-Air.local>
2017-10-22 22:02:03 +0100

    start menu to main game transition

---
+++ Musk2Mars/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	/* 	To make a button change scene, go to the on click() section in
+		in inspector, choose main camera, then ChangeScene.changeScene as the funtion.
+		Set the final value to the scene name you want it to swap to 
+	*/
+	
+	public void changeScene(string sceneName) {
+		SceneManager.LoadScene (sceneName );
+	}
+}
