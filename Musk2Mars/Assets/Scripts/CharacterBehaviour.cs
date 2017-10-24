d64a33aea4e1f3f4ed45fa714ed7922bfa0c9c02
Samuel Ogden <samuelogden@Samuels-MacBook-Air.local>
2017-10-24 22:30:11 +0100

    added end of game code to show ad option and main menu option

--- Musk2Mars/Assets/Scripts/CharacterBehaviour.cs
+++ Musk2Mars/Assets/Scripts/CharacterBehaviour.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterBehaviour : MonoBehaviour {
 
-	public float initialFuel;	//std fuel in rocket when start game
-	private float fuel;
-	public float moveSpeed;		//horizontal movement speed
+	public float initialFuel;			//std fuel in rocket when start game
+	public float moveSpeed;				//horizontal movement speed
 	public float maxVelocityChange;
+
+	public GameObject continuePlaying;	//UI Button that plays an ad so user can continue playing
+	public GameObject goToMainMenu;		//UI Button Return to main menu when game is over.
+
+
+	private float fuel;
+
 	// Use this for initialization
 	void Start () {
 		fuel = initialFuel;
+		goToMainMenu.SetActive(false);
+		continuePlaying.SetActive(false);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// If no more fuel, game is over.
+		if (fuel == 0)
+			outOfFuel ();
 		//calculate how fast player should be moving
 		Vector2 targetVelocityH = new Vector2(Input.acceleration.x, 0);
 		targetVelocityH = transform.TransformDirection(targetVelocityH);
@@ -28,5 +41,11 @@ public class CharacterBehaviour : MonoBehaviour {
 							-maxVelocityChange, maxVelocityChange);
 		GetComponent<Rigidbody2D>().AddForce(velocityChange,
 										ForceMode2D.Force);
+	}
+
+	// GAME OVER - Show view ad option to continue game
+	void outOfFuel() {
+		goToMainMenu.SetActive(true);
+		continuePlaying.SetActive(true);
 	}
 }
