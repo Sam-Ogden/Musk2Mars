5bdf1ba04a27237d54cb5747090acc050c7ecd15
fudgemcgroobs <tudor.cismarescu@gmail.com>
2017-10-24 14:56:29 +0100

    Initial Main Scene Setup
    Created ground and main character sprites. Added collision. Added a few mobile resolutions (not sure you can see) to make sure things are roughly in screen.

---
+++ Musk2Mars/Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour {
+
+	public float initialFuel;	//std fuel in rocket when start game
+	public float moveSpeed;		//horizontal movement speed
+	private float fuel;
+	// Use this for initialization
+	void Start () {
+		fuel = initialFuel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
