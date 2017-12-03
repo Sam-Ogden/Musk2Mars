6e3a361c4d2152a3d16e961b12a349f0a3378fac
Samuel Ogden <sam.george.ogden@gmail.com>
2017-12-03 13:04:02 +0000

    updated main menu withanimated tap to play text

---
+++ Musk2Mars/Assets/Scripts/TextAnmator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAnmator : MonoBehaviour {
+
+	public int duration;
+	public Text tapToPlay;
+	private Color color1 = Color.white;
+	private Color color2 = Color.grey;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float t = Mathf.PingPong(Time.time, duration) / duration;
+        tapToPlay.color = Color.Lerp(color1, color2, t);
+	}
+}
